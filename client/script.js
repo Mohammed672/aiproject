ece2858330e6c490da012d375b4e6c6501b15dc1
Mohammed672 <mohammed.altukmachi35@gmail.comm>
2024-01-07 15:08:00 -0500

    websitename

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
   try {
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codex-ai-0n0r.onrender.com', {
       method: 'POST',
       headers: {
         'content-type': 'application/json'
